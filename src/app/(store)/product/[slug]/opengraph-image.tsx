4935a56548e69b1fb221472503e35abf7c8c53cc
joschonarth <joschonarth@gmail.com>
2026-02-17 11:01:53 -0300

    feat: generate dynamic OpenGraph images for products

---
+++ src/app/(store)/product/[slug]/opengraph-image.tsx
@@ -0,0 +1,61 @@
+import { ImageResponse } from 'next/og'
+
+import { api } from '@/data/api'
+import { Product } from '@/data/types/product'
+import { env } from '@/env'
+
+export const runtime = 'edge'
+
+export const alt = ''
+
+export const size = {
+  width: 1200,
+  height: 630,
+}
+
+export const contentType = 'image/png'
+
+async function getProduct(slug: string): Promise<Product> {
+  const response = await api(`/products/${slug}`, {
+    next: {
+      revalidate: 60 * 15, // 15 minutes
+    },
+  })
+
+  const product = await response.json()
+
+  return product
+}
+
+export default async function OgImage({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+
+  const product = await getProduct(slug)
+
+  const productImageURL = new URL(product.image, env.APP_URL).toString()
+
+  return new ImageResponse(
+    <div
+      style={{
+        backgroundColor: '#09090b',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <img
+        src={productImageURL}
+        alt=""
+        width={1200}
+        height={630}
+        style={{ objectFit: 'cover' }}
+      />
+    </div>,
+    size,
+  )
+}
